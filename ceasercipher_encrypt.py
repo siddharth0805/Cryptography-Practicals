014e2c731b09212b04fcbea2eaed247190f8c341
siddharth0805 <87686864+siddharth0805@users.noreply.github.com>
2022-05-01 09:12:29 +0530

    Add files via upload

---
+++ ceasercipher_encrypt.py
@@ -0,0 +1,15 @@
+word=input("Enter a word : ")
+key=int(input("Enter the key : "))
+nwword=""
+ascii=nwa=0
+for i in word:
+    if  i.isupper():
+        ascii=ord(i)
+        nwa=(ascii+key-65)%26+65
+    elif i.islower():
+        ascii=ord(i)
+        nwa=(ascii+key-97)%26+97
+    else:
+        nwa=ord(i)
+    nwword+=chr(nwa)     
+print(nwword)
